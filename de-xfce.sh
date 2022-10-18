fe8da36d0389c79b718b51c8306c4bbcf4df4905
wahasa <69626847+wahasa@users.noreply.github.com>
2022-10-18 11:01:40 +0700

    Update de-xfce.sh

--- de-xfce.sh
+++ de-xfce.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apk add nano sudo
-apk add xfce4 xfce4-terminal xfce4-screensaver lightdm-gtk-greeter faenza-icon-theme adwaita-icon-theme firefox dbus bash
+apk add openssl nano sudo
+apk add xfce4 xfce4-terminal xfce4-screensaver lightdm-gtk-greeter faenza-icon-theme adwaita-icon-theme firefox dbus
 
 rm de-xfce.sh
