c94b845dd9d85b4e3c3780f7af636c5b5463c596
wahasa <69626847+wahasa@users.noreply.github.com>
2022-10-18 10:42:31 +0700

    Update de-xfce.sh

--- de-xfce.sh
+++ de-xfce.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 apk add nano sudo
-apk add xfce4 xfce4-terminal xfce4-screensaver lightdm-gtk-greeter adwaita-icon-theme dbus bash
-
-echo "#!/bin/sh
-export PULSE_SERVER=127.0.0.1
-DISPLAY=:1 startxfce4" > /usr/local/bin/vncstart
-
-chmod +x /usr/local/bin/vncstart
+apk add xfce4 xfce4-terminal xfce4-screensaver lightdm-gtk-greeter faenza-icon-theme adwaita-icon-theme firefox dbus bash
 
 rm de-xfce.sh
