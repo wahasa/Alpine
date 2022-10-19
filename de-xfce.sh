da2c86228afe61e0da61ae5c304c8dd7b761ff8c
wahasa <69626847+wahasa@users.noreply.github.com>
2022-10-19 09:24:30 +0700

    Update de-xfce.sh

--- de-xfce.sh
+++ de-xfce.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-apk add openssl nano sudo
-apk add xfce4 xfce4-terminal xfce4-screensaver lightdm-gtk-greeter faenza-icon-theme adwaita-icon-theme firefox dbus
+#Get the necessary components
+apk add nano sudo
+apk add --no-cache ca-certificates openssl bash-completion
+
+#Installing desktop environment
+apk add xfce4 xfce4-terminal faenza-icon-theme xfwm4-themes firefox dbus
+
+#Get the extra components
+apk add mousepad ristretto xfburn parole xfce4-notifyd xfce4-screenshooter xfce4-taskmanager xfce4-power-manager
 
 rm de-xfce.sh
