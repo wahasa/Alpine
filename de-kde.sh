67282eca5e244c8dd0b2f5856856b1adaa879b2f
wahasa <69626847+wahasa@users.noreply.github.com>
2022-12-05 09:23:12 +0700

    Update de-kde.sh

--- de-kde.sh
+++ de-kde.sh
@@ -5,8 +5,34 @@ apk add nano sudo
 apk add --no-cache ca-certificates openssl bash-completion
 
 #Installing desktop environment
-apk add plasma konsole dolphin pulseaudio firefox dbus 
+apk add plasma konsole dolphin pulseaudio dbus 
 
 #Get the extra components
+
+echo "#!/bin/sh
+export DISPLAY=:1
+export PULSE_SERVER=127.0.0.1
+rm -rf /run/dbus/dbus.pid
+dbus-daemon --system
+dbus-launch plasma_session" > /usr/local/bin/vncstart
+clear
+chmod +x /usr/local/bin/vncstart
+
+echo " "
+echo "Installing browser,.."
+echo " "
+apk add firefox
+clear
+echo " "
+echo "Vnc Server address will run at 127.0.0.1:5901"
+echo " "
+echo "In Termux"
+echo "Start Vnc Server, run vncstart"
+echo "Stop  Vnc Server, run vncstop"
+echo " "
+echo "In Linux"
+echo "Start Vnc, run vncstart"
+echo "Exit  Vnc, run ctrl+c"
+echo " "
 
 rm de-kde.sh
