49a7a7bcd6d88c5768baf1a1f29488077c006192
wahasa <69626847+wahasa@users.noreply.github.com>
2023-06-01 09:18:45 +0700

    Update alpine3.14.sh

--- Install/alpine3.14.sh
+++ Install/alpine3.14.sh
@@ -131,7 +131,7 @@ chmod +x $PREFIX/bin/$linux
 	echo ""
         echo "You can now start Alpine with 'alpine' script next time"
 	echo ""
-	#rm alpine3.14.sh
+	rm alpine3.14.sh
 else
 	echo "Installation unsuccessful"
 fi
