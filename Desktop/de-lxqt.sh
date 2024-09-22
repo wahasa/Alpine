3db27feab2c90469940ce7db3d839c95dadec745
wahasa <69626847+wahasa@users.noreply.github.com>
2024-09-22 21:14:25 +0700

    de-lxqt.sh

--- Desktop/de-lxqt.sh
+++ Desktop/de-lxqt.sh
@@ -15,23 +15,21 @@ export PULSE_SERVER=127.0.0.1
 rm -rf /run/dbus/dbus.pid
 dbus-launch startlxqt" > /usr/local/bin/vncstart
 chmod +x /usr/local/bin/vncstart
-
-clear
-echo ""
-echo "Installing browser,.."
-echo ""
-apk add firefox-esr
-clear
-echo ""
-echo "Vnc Server address will run at 127.0.0.1:5901"
-echo ""
-echo "In Termux"
-echo "Start Vnc Server, run vncstart"
-echo "Stop  Vnc Server, run vncstop"
-echo ""
-echo "In Linux"
-echo "Start Vnc, run vncstart"
-echo "Exit  Vnc, run ctrl+c"
-echo ""
-
+   clear
+   echo ""
+   echo "Installing browser,.."
+   echo ""
+   apk add firefox-esr
+   clear
+   echo ""
+   echo "Vnc Server address will run at 127.0.0.1:5901"
+   echo ""
+   echo "In Termux"
+   echo "Start Vnc Server, run vncstart"
+   echo "Stop  Vnc Server, run vncstop"
+   echo ""
+   echo "In Linux"
+   echo "Start Vnc, run vncstart"
+   echo "Exit Vnc, run ctrl+c 2x"
+   echo ""
 rm de-lxqt.sh
