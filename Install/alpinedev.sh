1186aeff55c40f529d37b2d6e35f7c3cb8277a9e
wahasa <69626847+wahasa@users.noreply.github.com>
2024-11-07 19:33:47 +0700

    alpinedev.sh

--- Install/alpinedev.sh
+++ Install/alpinedev.sh
@@ -2,54 +2,55 @@
 pkg install root-repo x11-repo
 pkg install proot xz-utils neofetch pulseaudio -y
 #termux-setup-storage
-linux=alpine
-alpine=edge
-build=20240923
-bin=.alpine
-   echo ""
-   neofetch --ascii_distro $linux
-   folder=alpine-fs
+alpine=3.20
+build=3
+echo ""
+neofetch --ascii_distro Alpine -L
+folder=alpine-fs
 if [ -d "$folder" ]; then
         first=1
         echo "Skipping Downloading."
-   fi
-   tarball="alpine-rootfs.tar.gz"
+fi
+tarball="alpine-rootfs.tar.gz"
 if [ "$first" != 1 ];then
-        if [ ! -f $tarball ]; then	
-           echo "Download Rootfs, this may take a while base on your internet speed."
-           case `dpkg --print-architecture` in
-	        aarch64)
-		        archurl="aarch64" ;;
-	        arm*)
-		        archurl="armhf" ;;
-	        i386)
-		        archurl="x86" ;;
-	        x86_64)
-		        archurl="x86_64" ;;
-	        *)
-		        echo "Unknown Architecture"; exit 1 ;;
-	        esac
-	        wget -q --show-progress "https://dl-cdn.alpinelinux.org/alpine/${alpine}/releases/${archurl}/alpine-minirootfs-${build}-${archurl}.tar.gz" -O $tarball	
+        if [ ! -f $tarball ]; then
+	echo "Download Rootfs, this may take a while base on your internet speed."
+	case `dpkg --print-architecture` in
+	aarch64)
+		archurl="aarch64" ;;
+	arm*)
+		archurl="armhf" ;;
+	i386)
+		archurl="x86" ;;
+	x86_64)
+		archurl="x86_64" ;;
+	*)
+		echo "Unknown Architecture."; exit 1 ;;
+	esac
+	wget -q --show-progress "https://dl-cdn.alpinelinux.org/alpine/v${alpine}/releases/${archurl}/alpine-minirootfs-${alpine}.${build}-${archurl}.tar.gz" -O $tarball
 	fi
         cur=`pwd`
         mkdir -p "$folder"
 	mkdir -p $folder/binds
+        mkdir -p $folder/dev/shm
         #cd "$folder"
-        echo "Decompressing Rootfs, please be patient."
-        proot --link2symlink  \
+	echo "Decompressing Rootfs, please be patient."
+        proot --link2symlink \
           tar --warning=no-unknown-keyword \
               --delay-directory-restore --preserve-permissions \
               -xzpf ~/${tarball} -C ~/$folder/ --strip-components=1 --exclude json --exclude VERSION --exclude='dev'||:
 	#proot --link2symlink tar -xzpf ${cur}/${tarball} --strip-components=1 --exclude json --exclude VERSION --exclude='dev'||:
         #tar -xzpf layer.tar ; rm layer.tar
 	#cd "$cur"
-   fi
-   echo "" > $folder/etc/fstab
-   echo "localhost" > ~/"$folder"/etc/hostname
-   echo "127.0.0.1 localhost" > ~/"$folder"/etc/hosts
-   echo "nameserver 8.8.8.8" > ~/"$folder"/etc/resolv.conf
-   echo ""
-   echo "Writing launch script"
+	fi
+     echo "" > ~/$folder/etc/fstab
+     echo "localhost" > ~/$folder/etc/hostname
+     echo "127.0.0.1 localhost" > ~/"$folder"/etc/hosts
+     echo "nameserver 8.8.8.8" > ~/"$folder"/etc/resolv.conf
+bin=.alpine
+linux=alpine
+echo ""
+echo "Writing launch script"
 cat > $bin <<- EOM
 #!/data/data/com.termux/files/usr/bin/bash
 cd \$(dirname \$0)
@@ -80,11 +81,11 @@ command+=" -b /proc/self/fd/1:/dev/stdout"
 command+=" -b /proc/self/fd/2:/dev/stderr"
 command+=" -b /sys"
 command+=" -b /data/data/com.termux/files/usr/tmp:/tmp"
-command+=" -b $folder/tmp:/dev/shm"
+#command+=" -b $folder/tmp:/dev/shm"
 command+=" -b $folder/root:/dev/shm"
 ## Uncomment the following line to have access to the home directory of termux
 command+=" -b /data/data/com.termux"
-command+=" -b /data/data/com.termux/files/home:/root"
+#command+=" -b /data/data/com.termux/files/home:/root"
 ## Uncomment the following line to mount /sdcard directly to /
 command+=" -b /sdcard"
 command+=" -b /mnt"
@@ -94,7 +95,7 @@ command+=" HOME=/root"
 command+=" PATH=/bin:/usr/bin:/sbin:/usr/sbin"
 command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
 command+=" TERM=\$TERM"
-command+=" LANG=C.UTF-8"
+#command+=" LANG=C.UTF-8"
 command+=" LANG=en_US.UTF-8"
 command+=" LC_ALL=C"
 command+=" LANGUAGE=en_US"
@@ -103,38 +104,59 @@ com="\$@"
 if [ -z "\$1" ];then
    exec \$command
 else
-   \$command -c "\$@"
+   #\$command -c "\$@"
    \$command -c "\$com"
 fi
 EOM
-    echo "Fixing shebang of $linux"
-    termux-fix-shebang $bin
-    echo "Making $linux executable"
-    chmod +x $bin
-    #echo "Fixing permissions for $linux"
-    #chmod -R 755 $folder
-    #echo "Removing image for some space"
-    #rm $tarball
-echo "export PULSE_SERVER=127.0.0.1" >> $folder/root/.bashrc
-echo '#!/bin/bash
-bash .alpine' > $PREFIX/bin/$linux
-chmod +x $PREFIX/bin/$linux
-    clear
-    echo ""
-    echo "Updating Alpine,.."
-    echo ""
+     echo ""
+     echo "Fixing shebang of $linux"
+     termux-fix-shebang $bin
+     echo "Making $linux executable"
+     chmod +x $bin
+     echo "Fixing permissions for $linux"
+     #chmod -R 755 $folder
+     echo "Removing image for some space"
+     #rm $tarball
+echo ""
+echo "#Alpine Repositories
+https://dl-cdn.alpinelinux.org/alpine/v3.20/main
+https://dl-cdn.alpinelinux.org/alpine/edge/testing
+https://dl-cdn.alpinelinux.org/alpine/v3.20/community" > ~/"$folder"/etc/apk/repositories
+     ./$bin apk update
+     ./$bin apk add --no-cache bash
+     sed -i 's/ash/bash/g' $folder/etc/passwd
+     sed -i 's/bin\/sh/bin\/bash/g' $bin
+     echo "export PULSE_SERVER=127.0.0.1" >> $folder/root/.bashrc
+     echo 'bash .alpine' > $PREFIX/bin/$linux
+     chmod +x $PREFIX/bin/$linux
+     clear
+     echo ""
+     echo "Updating Alpine,.."
+     echo ""
 echo "#!/bin/bash
 touch ~/.hushlogin
 apk update ; apk upgrade
-apk add nano sudo dialog
+apk add dialog nano sudo ncurses tzdata
+ln -s /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 rm -rf ~/.bash_profile
 exit" > $folder/root/.bash_profile
 bash $bin
-   clear
-   echo ""
-   echo "You can login to Alpine with 'alpine' script next time"
-   echo ""
-   #rm alpinedev.sh
- #
-### Script edited by 'WaHaSa', Script revision-5.
- #
+sleep 1
+echo 'PRETTY_NAME="Alpine Edge (Development Branch)"
+NAME="Alpine Linux"
+VERSION_ID="3.21"
+VERSION="3.20.0 (Development)"
+VERSION_CODENAME=edge
+ID=alpine
+HOME_URL="https://alpinelinux.org"
+SUPPORT_URL="https://alpinelinux.org/community"
+BUG_REPORT_URL="https://gitlab.alpinelinux.org/alpine/aports/-/issues"
+LOGO=alpinelinux-logo' > ~/"$folder"/etc/os-release
+     clear
+     echo ""
+     echo "You can login to Alpine with 'alpine' script next time"
+     echo ""
+     #rm alpinedev.sh
+#
+## Script edited by 'WaHaSa', Script revision-5.
+#
