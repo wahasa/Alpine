f88ec400fcba644034fc5c9f08d81878236dc6cc
wahasa <69626847+wahasa@users.noreply.github.com>
2024-11-07 16:42:42 +0700

    alpine.sh

--- Install/alpine.sh
+++ Install/alpine.sh
@@ -1,20 +1,19 @@
 #!/data/data/com.termux/files/usr/bin/bash
 pkg install root-repo x11-repo
 pkg install proot xz-utils neofetch pulseaudio -y
-termux-setup-storage
+#termux-setup-storage
 alpine=3.20
 build=3
 folder=alpine-fs
+neofetch --ascii_distro Alpine -L
+if [ -d "$folder" ]; then
+        first=1
+        echo "Skipping Downloading"
+fi
 tarball="alpine-rootfs.tar.gz"
-mkdir -p $folder $folder/binds
-[ -f $tarball ] && check=1
-if [ "$check" -eq "1" ] > /dev/null 2>&1; then
-	echo "Please Waiting,."
-	if [ -x "$(command -v neofetch)" ]; then
-		neofetch --ascii_distro Alpine -L
-	fi
-	pv $tarball | proot --link2symlink tar -zxf - -C $folder || :
-else
+if [ "$first" != 1 ];then
+        if [ ! -f $tarball ]; then
+	echo "Download Rootfs, this may take a while base on your internet speed"
 	case `dpkg --print-architecture` in
 	aarch64)
 		archurl="aarch64" ;;
@@ -27,86 +26,83 @@ else
 	*)
 		echo "Unknown Architecture"; exit 1 ;;
 	esac
-	url=https://dl-cdn.alpinelinux.org/alpine/v${alpine}/releases/${archurl}/alpine-minirootfs-${alpine}.${build}-${archurl}.tar.gz
-	echo "Downloading and Extracting Rootfs,."
-	echo ""
-	if [ -x "$(command -v neofetch)" ]; then
-		neofetch --ascii_distro Alpine -L
+	wget "https://dl-cdn.alpinelinux.org/alpine/v${alpine}/releases/${archurl}/alpine-minirootfs-${alpine}.${build}-${archurl}.tar.gz" -O $tarball
 	fi
-	wget -qO- --tries=0 $url --show-progress --progress=bar:force:noscroll |proot --link2symlink tar -zxf - -C $folder --exclude='dev' || :
-fi
+        cur=`pwd`
+        mkdir -p "$folder"
+	mkdir -p $folder/binds
+        cd "$folder"
+	echo "Decompressing Rootfs, please be patient."
+        proot --link2symlink  \
+          tar --warning=no-unknown-keyword \
+              --delay-directory-restore --preserve-permissions \
+              -xzpf ~/${tarball} -C ~/$folder/ --strip-components=1 --exclude json --exclude VERSION --exclude='dev'||:
+	#proot --link2symlink tar -xzpf ${cur}/${tarball} --strip-components=1 --exclude json --exclude VERSION --exclude='dev'||:
+        #tar -xzpf layer.tar ; rm layer.tar
+	cd "$cur"
+	fi
+        echo "localhost" > ~/"$folder"/etc/hostname
+	echo "127.0.0.1 localhost" > ~/"$folder"/etc/hosts
+        echo "nameserver 8.8.8.8" > ~/"$folder"/etc/resolv.conf
 bin=.alpine
 linux=alpine
-if [ -d $folder/var ];then
-	echo ""
-	echo "Writing launch script"
-	cat > $bin <<- EOM
+echo ""
+echo "Writing launch script"
+cat > $bin <<- EOM
 	#!/data/data/com.termux/files/usr/bin/bash
 pulseaudio --start \
     --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" \
     --exit-idle-time=-1
-	cd \$(dirname \$0)
-	## Unset LD_PRELOAD in case termux-exec is installed
-	unset LD_PRELOAD
-	command="proot"
-	command+=" --kill-on-exit"
-	command+=" --link2symlink"
-	command+=" -0"
-	command+=" -r $folder"
-	if [ -n "\$(ls -A $folder/binds)" ]; then
-    		for f in $folder/binds/* ;do
-      		. \$f
-    	done
-	fi
-	command+=" -b /dev"
-	command+=" -b /dev/null:/proc/sys/kernel/cap_last_cap"
-	command+=" -b /proc"
-	command+=" -b /data/data/com.termux/files/usr/tmp:/tmp"
-	command+=" -b $folder/root:/dev/shm"
-	## Uncomment the following line to have access to the home directory of termux
-	#command+=" -b /data/data/com.termux/files/home:/root"
-	## Uncomment the following line to mount /sdcard directly to /
-	command+=" -b /sdcard"
-	command+=" -w /root"
-	command+=" /usr/bin/env -i"
-	command+=" HOME=/root"
-	command+=" PATH=/bin:/usr/bin:/sbin:/usr/sbin"
-	command+=" TERM=\$TERM"
-	command+=" LANG=en_US.UTF-8"
-	command+=" LC_ALL=C"
-	command+=" LANGUAGE=en_US"
-	command+=" /bin/sh --login"
-	com="\$@"
-	if [ -z "\$1" ];then
-    		exec \$command
-	else
-    		\$command -c "\$com"
-	fi
-	EOM
-
-	if test -f "$bin"; then
+cd \$(dirname \$0)
+## Unset LD_PRELOAD in case termux-exec is installed
+unset LD_PRELOAD
+command="proot"
+command+=" --kill-on-exit"
+command+=" --link2symlink"
+command+=" -0"
+command+=" -r $folder"
+if [ -n "\$(ls -A $folder/binds)" ]; then
+   for f in $folder/binds/* ;do
+     . \$f
+   done
+fi
+command+=" -b /dev"
+command+=" -b /dev/null:/proc/sys/kernel/cap_last_cap"
+command+=" -b /proc"
+command+=" -b /data/data/com.termux/files/usr/tmp:/tmp"
+command+=" -b $folder/root:/dev/shm"
+## Uncomment the following line to have access to the home directory of termux
+#command+=" -b /data/data/com.termux/files/home:/root"
+## Uncomment the following line to mount /sdcard directly to /
+command+=" -b /sdcard"
+command+=" -w /root"
+command+=" /usr/bin/env -i"
+command+=" HOME=/root"
+command+=" PATH=/bin:/usr/bin:/sbin:/usr/sbin"
+command+=" TERM=\$TERM"
+command+=" LANG=en_US.UTF-8"
+command+=" LC_ALL=C"
+command+=" LANGUAGE=en_US"
+command+=" /bin/sh --login"
+com="\$@"
+if [ -z "\$1" ];then
+   exec \$command
+else
+   \$command -c "\$com"
+fi
+EOM
   	echo "Fixing shebang of $linux"
-		termux-fix-shebang $bin
-	fi
-
-	FD=$folder
-	if [ -d "$FD" ]; then
+	termux-fix-shebang $bin
 	echo "Making $linux executable"
-		chmod +x $bin
-	fi
-
-	UFD=$folder/binds
-	if [ -d "$UFD" ]; then
+	chmod +x $bin
 	echo "Removing image for some space"
-		#rm $tarball
-	fi
-
-	echo ""
-	echo "" > $folder/etc/fstab
-	echo "alpine" > ~/"$folder"/etc/hostname
-   	echo "127.0.0.1 localhost" > ~/"$folder"/etc/hosts
-	echo "nameserver 8.8.8.8" > ~/"$folder"/etc/resolv.conf
-        echo "#Alpine Repositories
+	#rm $tarball
+echo ""
+echo "" > $folder/etc/fstab
+#echo "alpine" > ~/"$folder"/etc/hostname
+#echo "127.0.0.1 localhost" > ~/"$folder"/etc/hosts
+#echo "nameserver 8.8.8.8" > ~/"$folder"/etc/resolv.conf
+echo "#Alpine Repositories
 https://dl-cdn.alpinelinux.org/alpine/v3.20/main
 https://dl-cdn.alpinelinux.org/alpine/v3.20/community" > ~/"$folder"/etc/apk/repositories
 	./$bin apk update
@@ -120,33 +116,33 @@ https://dl-cdn.alpinelinux.org/alpine/v3.20/community" > ~/"$folder"/etc/apk/rep
 	echo ""
 	echo "Updating Alpine,.."
 	echo ""
-	echo "#!/bin/bash
+echo "#!/bin/bash
 apk update ; apk upgrade
 apk add nano sudo dialog
 rm -rf ~/.bash_profile
 exit" > $folder/root/.bash_profile
-	bash $bin
-	echo 'PRETTY_NAME="Alpine 3.20 Linux"
-NAME="Alpine"
-VERSION_ID="3.20"
-VERSION="3.20.3"
-ID=alpine
-HOME_URL="https://alpinelinux.org"
-DOCUMENTATION_URL="https://wiki.alpinelinux.org"
-SUPPORT_URL="https://alpinelinux.org/community"
-BUG_REPORT_URL="https://gitlab.alpinelinux.org/alpine/aports/-/issues"
-PRIVACY_POLICY_URL="https://wiki.alpinelinux.org/wiki/Alpine_Linux:Privacy_policy"
-LOGO=alpinelinux-logo' > ~/"$folder"/etc/os-release
+bash $bin
+#echo 'PRETTY_NAME="Alpine 3.20 Linux"
+#NAME="Alpine"
+#VERSION_ID="3.20"
+#VERSION="3.20.3"
+#ID=alpine
+#HOME_URL="https://alpinelinux.org"
+#DOCUMENTATION_URL="https://wiki.alpinelinux.org"
+#SUPPORT_URL="https://alpinelinux.org/community"
+#BUG_REPORT_URL="https://gitlab.alpinelinux.org/alpine/aports/-/issues"
+#PRIVACY_POLICY_URL="https://wiki.alpinelinux.org/wiki/Alpine_Linux:Privacy_policy"
+#LOGO=alpinelinux-logo' > ~/"$folder"/etc/os-release
 	clear
 	echo ""
         echo "You can login to Alpine with 'alpine' script next time"
 	echo ""
 	#rm alpine3.20.sh
-else
-        echo ""
-	echo "Installation Unsuccessful"
-        echo ""
-fi
+#else
+#        echo ""
+#	echo "Installation Unsuccessful"
+#        echo ""
+#fi
  #
 ### Script edited by 'WaHaSa', Script revision-4.
  #
