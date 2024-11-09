5c3b1ebd3fff5503c4cceb88bc9ffd7ebb2c486f
wahasa <69626847+wahasa@users.noreply.github.com>
2024-11-09 09:28:10 +0700

    postmarketos23.12.sh

--- PostmarketOS/Install/postmarketos23.12.sh
+++ PostmarketOS/Install/postmarketos23.12.sh
@@ -1,20 +1,20 @@
 #!/data/data/com.termux/files/usr/bin/bash
 pkg install root-repo x11-repo
 pkg install proot xz-utils neofetch pulseaudio -y
-termux-setup-storage
+#termux-setup-storage
 alpine=3.19
 build=4
+     echo ""
+     neofetch --ascii_distro PostmarketOS -L
 folder=postmarketos-fs
+if [ -d "$folder" ]; then
+        first=1
+        echo "Skipping Downloading."
+fi
 tarball="postmarketos-rootfs.tar.gz"
-mkdir -p $folder $folder/binds
-[ -f $tarball ] && check=1
-if [ "$check" -eq "1" ] > /dev/null 2>&1; then
-	echo "Please Waiting,."
-	if [ -x "$(command -v neofetch)" ]; then
-		neofetch --ascii_distro PostmarketOS -L
-	fi
-	pv $tarball | proot --link2symlink tar -zxf - -C $folder || :
-else
+if [ "$first" != 1 ];then
+        if [ ! -f $tarball ]; then
+	echo "Download Rootfs, this may take a while base on your internet speed."
 	case `dpkg --print-architecture` in
 	aarch64)
 		archurl="aarch64" ;;
@@ -25,133 +25,140 @@ else
 	x86_64)
 		archurl="x86_64" ;;
 	*)
-		echo "Unknown Architecture"; exit 1 ;;
+		echo "Unknown Architecture."; exit 1 ;;
 	esac
-	url=https://dl-cdn.alpinelinux.org/alpine/v${alpine}/releases/${archurl}/alpine-minirootfs-${alpine}.${build}-${archurl}.tar.gz
-	echo "Downloading and Extracting Rootfs,."
-	echo ""
-	if [ -x "$(command -v neofetch)" ]; then
-		neofetch --ascii_distro PostmarketOS -L
+	wget -q --show-progress "https://dl-cdn.alpinelinux.org/alpine/v${alpine}/releases/${archurl}/alpine-minirootfs-${alpine}.${build}-${archurl}.tar.gz" -O $tarball
 	fi
-	wget -qO- --tries=0 $url --show-progress --progress=bar:force:noscroll |proot --link2symlink tar -zxf - -C $folder --exclude='dev' || :
-fi
+        cur=`pwd`
+        mkdir -p "$folder"
+	mkdir -p $folder/binds
+        mkdir -p $folder/dev/shm
+        #cd "$folder"
+	echo "Decompressing Rootfs, please be patient."
+        proot --link2symlink \
+          tar --warning=no-unknown-keyword \
+              --delay-directory-restore --preserve-permissions \
+              -xzpf ~/${tarball} -C ~/$folder/ --strip-components=1 --exclude json --exclude VERSION --exclude='dev'||:
+	#proot --link2symlink tar -xzpf ${cur}/${tarball} --strip-components=1 --exclude json --exclude VERSION --exclude='dev'||:
+        #tar -xzpf layer.tar ; rm layer.tar
+	#cd "$cur"
+	fi
+     echo "" > ~/$folder/etc/fstab
+     echo "localhost" > ~/$folder/etc/hostname
+     echo "127.0.0.1 localhost" > ~/"$folder"/etc/hosts
+     echo "nameserver 8.8.8.8" > ~/"$folder"/etc/resolv.conf
 bin=.postmarketos
 linux=postmarketos
-if [ -d $folder/var ];then
-	echo ""
-	echo "Writing launch script"
-	cat > $bin <<- EOM
-	#!/data/data/com.termux/files/usr/bin/bash
-pulseaudio --start \
-    --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" \
-    --exit-idle-time=-1
-	cd \$(dirname \$0)
-	## Unset LD_PRELOAD in case termux-exec is installed
-	unset LD_PRELOAD
-	command="proot"
-	command+=" --kill-on-exit"
-	command+=" --link2symlink"
-	command+=" -0"
-	command+=" -r $folder"
-	if [ -n "\$(ls -A $folder/binds)" ]; then
-    		for f in $folder/binds/* ;do
-      		. \$f
-    	done
-	fi
-	command+=" -b /dev"
-	command+=" -b /dev/null:/proc/sys/kernel/cap_last_cap"
-	command+=" -b /proc"
-	command+=" -b /data/data/com.termux/files/usr/tmp:/tmp"
-	command+=" -b $folder/root:/dev/shm"
-	## Uncomment the following line to have access to the home directory of termux
-	#command+=" -b /data/data/com.termux/files/home:/root"
-	## Uncomment the following line to mount /sdcard directly to /
-	command+=" -b /sdcard"
-	command+=" -w /root"
-	command+=" /usr/bin/env -i"
-	command+=" HOME=/root"
-	command+=" PATH=/bin:/usr/bin:/sbin:/usr/sbin"
-	command+=" TERM=\$TERM"
-	command+=" LANG=en_US.UTF-8"
-	command+=" LC_ALL=C"
-	command+=" LANGUAGE=en_US"
-	command+=" /bin/sh --login"
-	com="\$@"
-	if [ -z "\$1" ];then
-    		exec \$command
-	else
-    		\$command -c "\$com"
-	fi
-	EOM
-
-	if test -f "$bin"; then
-  	echo "Fixing shebang of $linux"
-		termux-fix-shebang $bin
-	fi
-
-	FD=$folder
-	if [ -d "$FD" ]; then
-	echo "Making $linux executable"
-		chmod +x $bin
-	fi
-
-	UFD=$folder/binds
-	if [ -d "$UFD" ]; then
-	echo "Removing image for some space"
-		#rm $tarball
-	fi
-
-	echo ""
-	echo "" > $folder/etc/fstab
-	echo "alpine" > ~/"$folder"/etc/hostname
-   	echo "127.0.0.1 localhost" > ~/"$folder"/etc/hosts
-	echo "nameserver 8.8.8.8" > ~/"$folder"/etc/resolv.conf
-        echo "#Alpine Repositories
+echo ""
+echo "Writing launch script"
+cat > $bin <<- EOM
+#!/data/data/com.termux/files/usr/bin/bash
+cd \$(dirname \$0)
+## Start pulseaudio
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+## Unset LD_PRELOAD in case termux-exec is installed
+unset LD_PRELOAD
+## Uncomment following line if you are having FATAL: kernel too old message
+#command+=" -k 4.14.81"
+command="proot"
+command+=" --kill-on-exit"
+command+=" --link2symlink"
+command+=" -0"
+command+=" -r $folder"
+if [ -n "\$(ls -A $folder/binds)" ]; then
+   for f in $folder/binds/* ;do
+     . \$f
+   done
+fi
+command+=" -b /dev"
+command+=" -b /dev/null:/proc/sys/kernel/cap_last_cap"
+command+=" -b /dev/null:/proc/stat"
+command+=" -b /dev/urandom:/dev/random"
+command+=" -b /proc"
+command+=" -b /proc/self/fd:/dev/fd"
+command+=" -b /proc/self/fd/0:/dev/stdin"
+command+=" -b /proc/self/fd/1:/dev/stdout"
+command+=" -b /proc/self/fd/2:/dev/stderr"
+command+=" -b /sys"
+command+=" -b /data/data/com.termux/files/usr/tmp:/tmp"
+#command+=" -b $folder/tmp:/dev/shm"
+command+=" -b $folder/root:/dev/shm"
+## Uncomment the following line to have access to the home directory of termux
+command+=" -b /data/data/com.termux"
+#command+=" -b /data/data/com.termux/files/home:/root"
+## Uncomment the following line to mount /sdcard directly to /
+command+=" -b /sdcard"
+command+=" -b /mnt"
+command+=" -w /root"
+command+=" /usr/bin/env -i"
+command+=" HOME=/root"
+command+=" PATH=/bin:/usr/bin:/sbin:/usr/sbin"
+command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
+command+=" TERM=\$TERM"
+#command+=" LANG=C.UTF-8"
+command+=" LANG=en_US.UTF-8"
+command+=" LC_ALL=C"
+command+=" LANGUAGE=en_US"
+command+=" /bin/sh --login"
+com="\$@"
+if [ -z "\$1" ];then
+   exec \$command
+else
+   #\$command -c "\$@"
+   \$command -c "\$com"
+fi
+EOM
+     echo ""
+     echo "Fixing shebang of $linux"
+     termux-fix-shebang $bin
+     echo "Making $linux executable"
+     chmod +x $bin
+     echo "Fixing permissions for $linux"
+     #chmod -R 755 $folder
+     echo "Removing image for some space"
+     #rm $tarball
+echo ""
+echo "#PostmarketOS Repositories
 https://mirror.postmarketos.org/postmarketos/v23.12
 https://dl-cdn.alpinelinux.org/alpine/v3.19/main
 https://dl-cdn.alpinelinux.org/alpine/v3.19/community" > ~/"$folder"/etc/apk/repositories
-	./$bin apk update
-        ./$bin apk add --no-cache bash
-        sed -i 's/ash/bash/g' $folder/etc/passwd
-        sed -i 's/bin\/sh/bin\/bash/g' $bin
-	echo "export PULSE_SERVER=127.0.0.1" >> $folder/root/.bashrc
-	echo 'bash .postmarketos' > $PREFIX/bin/$linux
-	chmod +x $PREFIX/bin/$linux
-	clear
-	echo ""
-	echo "Updating PostmarketOS,.."
-	echo ""
-	echo "#!/bin/bash
+     ./$bin apk update
+     ./$bin apk add --no-cache bash
+     sed -i 's/ash/bash/g' $folder/etc/passwd
+     sed -i 's/bin\/sh/bin\/bash/g' $bin
+     echo "export PULSE_SERVER=127.0.0.1" >> $folder/root/.bashrc
+     echo 'bash .alpine' > $PREFIX/bin/$linux
+     chmod +x $PREFIX/bin/$linux
+     clear
+     echo ""
+     echo "Updating PostmarketOS,.."
+     echo ""
+echo "#!/bin/bash
+touch ~/.hushlogin
 apk add -u --allow-untrusted postmarketos-keys
 apk update ; apk upgrade
-apk add nano sudo dialog
+apk add dialog nano sudo ncurses tzdata
+ln -s /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 rm -rf ~/.bash_profile
 exit" > $folder/root/.bash_profile
-	bash $bin
-	echo 'PRETTY_NAME="PostmarketOS 23.12 Linux"
+bash $bin
+sleep 1
+echo 'PRETTY_NAME="PostmarketOS 23.12 Linux"
 NAME="PostmarketOS"
 VERSION_ID="23.12"
 VERSION="23.12"
 ID=postmarketos
 ID_LIKE=alpine
 HOME_URL="https://postmarketos.org"
-DOCUMENTATION_URL="https://wiki.postmarketos.org"
-SUPPORT_URL="https://wiki.postmarketos.org/wiki/Contributing"
+SUPPORT_URL="https://gitlab.postmarketos.org/postmarketOS"
 BUG_REPORT_URL="https://gitlab.com/postmarketOS/pmaports/-/issues"
-PRIVACY_POLICY_URL="https://postmarketos.org/privacy-policy"
 ALPINE_VERSION="3.19"
 LOGO=postmarketos-logo' > ~/"$folder"/etc/os-release
-	clear
-	echo ""
-        echo "You can login to PostmarketOS with 'postmarketos' script next time"
-	echo ""
-	#rm postmarketos23.12.sh
-else
-        echo ""
-	echo "Installation Unsuccessful"
-        echo ""
-fi
- #
-### Script edited by 'WaHaSa', Script revision-4.
- #
-
+     clear
+     echo ""
+     echo "You can login to PostmarketOS with 'postmarketos' script next time"
+     echo ""
+     #rm postmarketos23.12.sh
+##
+## Script edited by 'WaHaSa', Script revision-5.
+##
