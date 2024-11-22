bc5f14076b8ca9a1c904c1b8d6313a6fc5547e58
wahasa <69626847+wahasa@users.noreply.github.com>
2024-11-22 09:59:45 +0700

    alpinedev.sh

--- Install/alpinedev.sh
+++ Install/alpinedev.sh
@@ -4,49 +4,41 @@ pkg install proot xz-utils neofetch pulseaudio -y
 #termux-setup-storage
 alpine=edge
 build=20240923
-     echo ""
-     neofetch --ascii_distro Alpine -L
+   echo ""
+   neofetch --ascii_distro Alpine -L
 folder=alpine-fs
 if [ -d "$folder" ]; then
-        first=1
-        echo "Skipping Downloading."
+	 first=1
+         echo "Skipping Downloading."
 fi
 tarball="alpine-rootfs.tar.gz"
 if [ "$first" != 1 ];then
-        if [ ! -f $tarball ]; then
-	echo "Download Rootfs, this may take a while base on your internet speed."
-	case `dpkg --print-architecture` in
-	aarch64)
-		archurl="aarch64" ;;
-	arm*)
-		archurl="armhf" ;;
-	i386)
-		archurl="x86" ;;
-	x86_64)
-		archurl="x86_64" ;;
-	*)
-		echo "Unknown Architecture."; exit 1 ;;
-	esac
-	wget -q --show-progress "https://dl-cdn.alpinelinux.org/alpine/${alpine}/releases/${archurl}/alpine-minirootfs-${build}-${archurl}.tar.gz" -O $tarball
-	fi
-        cur=`pwd`
-        mkdir -p "$folder"
-	mkdir -p $folder/binds
-        mkdir -p $folder/dev/shm
-        #cd "$folder"
-	echo "Decompressing Rootfs, please be patient."
-        proot --link2symlink \
-          tar --warning=no-unknown-keyword \
-              --delay-directory-restore --preserve-permissions \
-              -xpf ~/${tarball} -C ~/$folder/ --strip-components=1 --exclude json --exclude VERSION --exclude='dev'||:
-	#proot --link2symlink tar -xpf ${cur}/${tarball} --strip-components=1 --exclude json --exclude VERSION --exclude='dev'||:
-        #tar -xpf layer.tar ; rm layer.tar
-	#cd "$cur"
-	fi
-     echo "" > ~/$folder/etc/fstab
-     echo "localhost" > ~/$folder/etc/hostname
-     echo "127.0.0.1 localhost" > ~/"$folder"/etc/hosts
-     echo "nameserver 8.8.8.8" > ~/"$folder"/etc/resolv.conf
+         if [ ! -f $tarball ]; then
+	       echo "Download Rootfs, this may take a while base on your internet speed."
+	       case `dpkg --print-architecture` in
+	       aarch64)
+		       archurl="aarch64" ;;
+	       arm*)
+		       archurl="armhf" ;;
+	       i386)
+		       archurl="x86" ;;
+	       x86_64)
+		       archurl="x86_64" ;;
+	       *)
+		       echo "Unknown Architecture."; exit 1 ;;
+	       esac
+	       wget -q --show-progress "https://dl-cdn.alpinelinux.org/alpine/${alpine}/releases/${archurl}/alpine-minirootfs-${build}-${archurl}.tar.gz" -O $tarball
+	 fi
+         mkdir -p $folder
+	 mkdir -p $folder/binds
+         mkdir -p $folder/dev/shm
+	 echo "Decompressing Rootfs, please be patient."
+         proot --link2symlink tar -xpf ~/${tarball} -C ~/$folder/ --exclude='dev'||:
+    fi
+    echo "" > $folder/etc/fstab
+    echo "localhost" > $folder/etc/hostname
+    echo "127.0.0.1 localhost" > $folder/etc/hosts
+    echo "nameserver 8.8.8.8" > $folder/etc/resolv.conf
 bin=.alpine
 linux=alpine
 echo ""
@@ -65,7 +57,7 @@ command+=" -0"
 command+=" -r $folder"
 if [ -n "\$(ls -A $folder/binds)" ]; then
    for f in $folder/binds/* ;do
-     . \$f
+       . \$f
    done
 fi
 command+=" -b /dev"
@@ -79,10 +71,8 @@ command+=" -b /proc/self/fd/1:/dev/stdout"
 command+=" -b /proc/self/fd/2:/dev/stderr"
 command+=" -b /sys"
 command+=" -b /data/data/com.termux/files/usr/tmp:/tmp"
-#command+=" -b $folder/tmp:/dev/shm"
 command+=" -b $folder/root:/dev/shm"
 ## Uncomment the following line to have access to the home directory of termux
-command+=" -b /data/data/com.termux"
 #command+=" -b /data/data/com.termux/files/home:/root"
 ## Uncomment the following line to mount /sdcard directly to /
 command+=" -b /sdcard"
@@ -90,19 +80,15 @@ command+=" -b /mnt"
 command+=" -w /root"
 command+=" /usr/bin/env -i"
 command+=" HOME=/root"
-command+=" PATH=/bin:/usr/bin:/sbin:/usr/sbin"
 command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
 command+=" TERM=\$TERM"
-#command+=" LANG=C.UTF-8"
-command+=" LANG=en_US.UTF-8"
 command+=" LC_ALL=C"
-command+=" LANGUAGE=en_US"
-command+=" /bin/sh --login"
+command+=" LANG=C.UTF-8"
+command+=" /bin/bash --login"
 com="\$@"
 if [ -z "\$1" ];then
    exec \$command
 else
-   #\$command -c "\$@"
    \$command -c "\$com"
 fi
 EOM
@@ -114,13 +100,12 @@ EOM
      echo "Fixing permissions for $linux"
      #chmod -R 755 $folder
      echo "Removing image for some space"
-     #rm $tarball
-echo ""
+     rm $tarball
+     echo ""
 echo "#Alpine Repositories
 https://dl-cdn.alpinelinux.org/alpine/edge/main
 https://dl-cdn.alpinelinux.org/alpine/edge/testing
-https://dl-cdn.alpinelinux.org/alpine/edge/community" > ~/"$folder"/etc/apk/repositories
-     ./$bin apk update
+https://dl-cdn.alpinelinux.org/alpine/edge/community" > $folder/etc/apk/repositories
      ./$bin apk add --no-cache bash
      sed -i 's/ash/bash/g' $folder/etc/passwd
      sed -i 's/bin\/sh/bin\/bash/g' $bin
@@ -139,17 +124,6 @@ ln -s /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 rm -rf ~/.bash_profile
 exit" > $folder/root/.bash_profile
      bash $bin
-     sleep 2
-echo 'PRETTY_NAME="Alpine Edge (Development Branch)"
-NAME="Alpine Linux"
-VERSION_ID="3.21"
-VERSION="3.21.0 (Development)"
-VERSION_CODENAME=edge
-ID=alpine
-HOME_URL="https://alpinelinux.org"
-SUPPORT_URL="https://alpinelinux.org/community"
-BUG_REPORT_URL="https://gitlab.alpinelinux.org/alpine/aports/-/issues"
-LOGO=alpinelinux-logo' > ~/"$folder"/etc/os-release
      clear
      echo ""
      echo "You can login to Alpine with 'alpine' script next time"
@@ -157,4 +131,4 @@ LOGO=alpinelinux-logo' > ~/"$folder"/etc/os-release
      #rm alpinedev.sh
 #
 ## Script edited by 'WaHaSa', Script revision-5.
-#
+##
