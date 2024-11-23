beddff630c8ec8d6a1601f583e419533208955f4
wahasa <69626847+wahasa@users.noreply.github.com>
2024-11-23 19:54:35 +0700

    alpine3.20.sh

--- Install/alpine3.20.sh
+++ Install/alpine3.20.sh
@@ -6,10 +6,10 @@ alpine=3.20
 build=3
    echo ""
    neofetch --ascii_distro Alpine -L
-folder=fedora-fs
+folder=alpine-fs
 if [ -d "$folder" ]; then
-        first=1
-        echo "Skipping Downloading."
+         first=1
+         echo "Skipping Downloading."
 fi
 tarball="alpine-rootfs.tar.gz"
 if [ "$first" != 1 ];then
@@ -106,9 +106,9 @@ https://dl-cdn.alpinelinux.org/alpine/v3.20/community" > $folder/etc/apk/reposit
 echo "export PULSE_SERVER=127.0.0.1" >> $folder/root/.bashrc
 echo 'bash .alpine' > $PREFIX/bin/$linux
 chmod +x $PREFIX/bin/$linux
-     ./$bin apk add --no-cache bash
      sed -i 's/ash/bash/g' $folder/etc/passwd
      sed -i 's/bin\/sh/bin\/bash/g' $bin
+     ./$bin apk add --no-cache bash
      clear
      echo ""
      echo "Updating Alpine,.."
