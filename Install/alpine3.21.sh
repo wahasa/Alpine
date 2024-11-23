e0c1edb4b8beb2498acdf473e4404686ac6717be
wahasa <69626847+wahasa@users.noreply.github.com>
2024-11-23 20:32:35 +0700

    alpine3.21.sh

--- Install/alpine3.21.sh
+++ Install/alpine3.21.sh
@@ -106,9 +106,9 @@ https://dl-cdn.alpinelinux.org/alpine/v3.21/community" > $folder/etc/apk/reposit
 echo "export PULSE_SERVER=127.0.0.1" >> $folder/root/.bashrc
 echo 'bash .alpine' > $PREFIX/bin/$linux
 chmod +x $PREFIX/bin/$linux
+     ./$bin apk add --no-cache bash
      sed -i 's/ash/bash/g' $folder/etc/passwd
      sed -i 's/bin\/sh/bin\/bash/g' $bin
-     ./$bin apk add --no-cache bash
      clear
      echo ""
      echo "Updating Alpine,.."
