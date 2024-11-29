1f9d91f06e2dfa5a3e0a00c69bd2aa0622499181
wahasa <69626847+wahasa@users.noreply.github.com>
2024-11-29 10:33:58 +0700

    alpinedev.sh

--- Install/alpinedev.sh
+++ Install/alpinedev.sh
@@ -2,10 +2,10 @@
 pkg install root-repo x11-repo
 pkg install proot xz-utils neofetch pulseaudio -y
 #termux-setup-storage
+echo ""
 alpine=edge
 build=20240923
-   echo ""
-   neofetch --ascii_distro Alpine -L
+neofetch --ascii_distro Alpine -L
 folder=alpine-fs
 if [ -d "$folder" ]; then
          first=1
@@ -34,12 +34,12 @@ if [ "$first" != 1 ];then
          echo "Decompressing Rootfs, please be patient."
          proot --link2symlink tar -xpf ~/${tarball} -C ~/$folder/ --exclude='dev'||:
     fi
+    echo ""
     echo "localhost" > $folder/etc/hostname
     echo "127.0.0.1 localhost" > $folder/etc/hosts
     echo "nameserver 8.8.8.8" > $folder/etc/resolv.conf
 bin=.alpine
 linux=alpine
-echo ""
 echo "Writing launch script"
 cat > $bin <<- EOM
 #!/data/data/com.termux/files/usr/bin/bash
@@ -99,11 +99,12 @@ EOM
      #chmod -R 755 $folder
      echo "Removing image for some space"
      rm $tarball
-     echo ""
+echo ""
 echo "#Alpine Repositories
 https://dl-cdn.alpinelinux.org/alpine/edge/main
 https://dl-cdn.alpinelinux.org/alpine/edge/testing
 https://dl-cdn.alpinelinux.org/alpine/edge/community" > $folder/etc/apk/repositories
+echo "" > $folder/root/.hushlogin
 echo "export PULSE_SERVER=127.0.0.1" >> $folder/root/.bashrc
 echo 'bash .alpine' > $PREFIX/bin/$linux
 chmod +x $PREFIX/bin/$linux
@@ -112,21 +113,20 @@ chmod +x $PREFIX/bin/$linux
      sed -i 's/bin\/sh/bin\/bash/g' $bin
      clear
      echo ""
-     echo "Updating Alpine,.."
+     echo "Updating Package,.."
      echo ""
 echo "#!/bin/bash
-touch ~/.hushlogin
 apk update ; apk upgrade
 apk add dialog nano sudo ncurses tzdata
 ln -s /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 rm -rf ~/.bash_profile
 exit" > $folder/root/.bash_profile
-     bash $bin
+bash $bin
      clear
      echo ""
-     echo "You can login to Alpine with 'alpine' script next time"
+     echo "You can login to Linux with 'alpine' script next time"
      echo ""
      #rm alpinedev.sh
 #
 ## Script edited by 'WaHaSa', Script revision-5.
-##
+#
