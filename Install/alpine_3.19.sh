5b625e9fb800fe8a06c2ff2d90d95c930bae61fe
wahasa <69626847+wahasa@users.noreply.github.com>
2025-03-12 09:25:41 +0700

    alpine_3.19.sh

--- Install/alpine_3.19.sh
+++ Install/alpine_3.19.sh
@@ -27,7 +27,7 @@ if [ "$first" != 1 ];then
                *)
                        echo "Unknown Architecture."; exit 1 ;;
                esac
-	       wget -q --show-progress "https://dl-cdn.alpinelinux.org/alpine/v${alpine}/releases/${archurl}/alpine-minirootfs-${alpine}.${build}-${archurl}.tar.gz" -O $tarball
+	       wget "https://dl-cdn.alpinelinux.org/alpine/v${alpine}/releases/${archurl}/alpine-minirootfs-${alpine}.${build}-${archurl}.tar.gz" -O $tarball
 	 fi
          mkdir -p $folder
 	 mkdir -p $folder/binds
